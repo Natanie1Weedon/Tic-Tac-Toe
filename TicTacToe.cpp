291a765a9faa459fceec6ad2600d78d789be0248
Natanie1Weedon <nataniel.weedon11@gmail.com>
2026-08-31 20:36:17 +0200

    Ukończenie planszy. Rozpoczęcie prac nad ruchami graczy i wstawianiem zmian do planszy po wykonaniu ruchu

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 char plansza[11][11];
 int main()
 {
-    int a,b,c;
+    void ustawKursor(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+    char kółko='O',krzyżyk='X',kolej,wygrana;
+    string ruch;
     for(int j=0;j<11;j++)
     {
         for(int i=0;i<11;i++)
         {
-           if(plansza[j][i]!=plansza[j][3]&&plansza[j][i]!=plansza[3][i]){plansza[j][i]='0';}
-           if(plansza[j][i]!=plansza[j][8]&&plansza[j][i]!=plansza[8][i]){plansza[j][i]='0';}
-           else{plansza[j][i]='#';}
+           if(i==3||i==7){plansza[j][i]='#' ;}
+           else if(j==3||j==7){plansza[j][i]='#' ;}
+           else{plansza[j][i]='0';}
         }
     }
-    for(int j=0;j<11;j++)
+    for(int k=0;k<9;k++)
     {
-        for(int i=0;i<11;i++)
+        ustawKursor(0,0)
+        cout<<"Cross starts/Krzyżyk zaczyna"<<endl<<"Type the space where to go/Wpisz pole gdzie iść"<<endl<<"The spaces are/Pola to:"<<endl;
+        cout<<"Centre/Środek  Top/Góra  LeftTop/LewaGóra  RightTop/PrawaGóra  Left/Lewo  Right/Prawo  Bottom/Dół  BottomLeft/LewyDół  RightBottom/PrawyDół"<<endl;
+        cout<<"(Use only the written ones or the computer wont read them/Użyj tylko te podane bo innaczej komputer nie przczyta)"<<endl<<endl;
+        for(int j=0;j<11;j++)
         {
-            cout<<plansza[j][i];
+            for(int i=0;i<11;i++)
+            {
+                cout<<plansza[j][i];
+            }
+            cout<<endl;
         }
-        cout<<endl;
+        if(k%2==0){kolej=krzyżyk;}
+        else if(k%2!=0){kolej=kółko;}
+        cin>>ruch;
+        if(ruch=="Centre"||ruch=="Środek")
+        {
+            if(kolej==krzyżyk)
+            {
+                plansza[4][4]='\\';
+                plansza[4][6]='/';
+                plansza[5][5]='/';
+                plansza[6][4]='/';
+                plansza[6][6]='\\';
+            }
+        }
     }
 }
